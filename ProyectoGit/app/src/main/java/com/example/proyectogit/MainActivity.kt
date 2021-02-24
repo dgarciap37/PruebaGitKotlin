e5567577f148ac6648104b20888744f30d85c193
RAFA <rafagutierrez14@gmail.com>
2021-02-24 16:00:50 +0100

    Segunda Version

--- ProyectoGit/app/src/main/java/com/example/proyectogit/MainActivity.kt
+++ ProyectoGit/app/src/main/java/com/example/proyectogit/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var count:  Int =10
+        var count2:  Int =50
+        var count1:  Int =50
     }
 }
